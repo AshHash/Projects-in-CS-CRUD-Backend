f9454e43fe35d67e8c0ce34003a2f7893a952a63
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2020-07-15 14:19:11 -0400

    closes #1 closes #2 fixed the issues that prevented the db from seeding upon initial setup and removed force sync

--- app.js
+++ app.js
@@ -10,19 +10,29 @@ const createLocalDatabase = require('./utilities/createLocalDatabase')
 const sync = async() => {
     console.log('As a reminder, the forced synchronization option is on');
     try {
-        await db.drop();
-        await db.sync({ force: true });
-        await seedDatabase();
+        await db.sync({ force: false });
     } catch (err) {
         if (err.name === 'SequelizeConnectionError') {
-            await createLocalDatabase()
-                .then(() => seedDatabase());
+            await createLocalDatabase();
+            await db.sync({ force: true });
+            await seedDatabase();
         } else {
             console.log(err);
         }
     }
 }
 
+//function for restarting the server using a child process. used for debugging
+// const restart = () =>{
+//     process.on("exit", function () {
+//         require("child_process").spawn(process.argv.shift(), process.argv, {
+//             cwd: process.cwd(),
+//             detached : true,
+//             stdio: "inherit"
+//         });
+//     });
+//     process.exit();
+// }
 
 const utils = async() => {
     app.use((req, res, next) => {
